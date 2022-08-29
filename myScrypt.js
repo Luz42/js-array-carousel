746a3ddf8bd51b5f1ee638c6a5408483770819c8
Luz42 <a.evangelista93@gmail.com>
2022-08-29 20:54:48 +0200

    STEP2 è stato assegnato l'evento al click dei bottoni per cambiare classe agli elementi  e far scorrere le immagini

--- myScrypt.js
+++ myScrypt.js
@@ -18,7 +18,62 @@ ma costruito dinamicamente attraverso JavaScript.
 MILESTONE 3
 Al click dell'utente sulle frecce, il programma cambierà l’immagine attiva, 
 che quindi verrà visualizzata al posto della precedente.
+
+Consigli del giorno:
+
+1. Costruiamo del carosello una versione statica contenente solamente un'immagine. 
+Di questa versione statica al momento opportuno commenteremo (oscureremo) alcuni elementi
+per poterli riprodurre dinamicamente in js. 
+Potremo quindi usarli come "template".
+
+2. Scriviamo sempre prima per punti il nostro algoritmo in italiano per capire cosa vogliamo fare
+
+3. Al momento giusto (ihihhi starà a voi capire quale) rispondete a questa domanda: "Quanti cicli servono?"
+
 */
 
-const images = ['01', '02', '03', '04', '05']
+
+//al click devono scorrere le immagini
+
+const nextButton = document.getElementById('next')
+const previousButton = document.getElementById('previous')
+
+nextButton.addEventListener('click',
+    function(){
+        
+        console.log('avanti')
+        const activeElement = document.querySelector('.img-fluid.w-100.d-block')
+        const nextElement = activeElement.nextElementSibling
+        console.log(nextElement)
+
+        if(nextElement){
+        activeElement.classList.add('d-none')
+        activeElement.classList.remove('d-block')
+        nextElement.classList.remove('d-none')
+        nextElement.classList.add('d-block')
+        }
+
+    }
+);
+
+previousButton.addEventListener('click',
+    function(){
+
+        console.log('indietro')
+        
+        const activeElement = document.querySelector('.img-fluid.w-100.d-block')
+        const previousElement = activeElement.previousElementSibling
+        console.log(previousElement)
+
+        if(previousElement){
+        activeElement.classList.add('d-none')
+        activeElement.classList.remove('d-block')
+        previousElement.classList.remove('d-none')
+        previousElement.classList.add('d-block')
+        }
+    }
+
+);
+//dalla immagine corrente,
+//quella successiva deve cambiare classe
 
